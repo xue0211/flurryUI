44772e5e245a6e0069b9808666447befa5453a9c
xue0211 <zhengtai1123.qq.com>
2025-02-03 19:50:03 +0800

    Button组件添加theme属性

--- flurryUI/src/components/ButtonDemo.vue
+++ flurryUI/src/components/ButtonDemo.vue
@@ -1,5 +1,23 @@
 <template>
+    <div>Button 示例</div>
+    <h1>示例1</h1>
     <div>
-        Button 文档
+        <Button>你好 </Button>
+        <Button theme="button">你好 </Button>
+        <Button theme="link">你好 </Button>
+        <Button theme="text">你好 </Button>
     </div>
 </template>
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+    components: { Button },
+    setup() {
+        const onClick = () => {
+            console.log(111);
+        };
+        return { onClick };
+    },
+};
+</script>
+<style lang="scss" scoped></style>
