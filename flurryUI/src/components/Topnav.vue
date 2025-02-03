c590a39053f021a319ee935cfb52a6f02b0e1171
xue0211 <zhengtai1123.qq.com>
2025-02-03 15:36:27 +0800

    完善路由切换

--- flurryUI/src/components/Topnav.vue
+++ flurryUI/src/components/Topnav.vue
@@ -22,11 +22,13 @@ export default {
 </script>
 <style lang="scss" scoped>
 .topnav {
-  position: relative;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
   background-color: pink;
   display: flex;
   padding: 16px;
-  position: relative;
   z-index: 10;
   justify-content: center;
   align-items: center;
--- flurryUI/src/views/Doc.vue
+++ flurryUI/src/views/Doc.vue
@@ -1,19 +1,21 @@
 <template>
     <div>
-        <Topnav />
-        <div class="content">
-            <aside v-if="asideVisible">
-                <h2>组件列表</h2>
-                <ol>
-                    <li><router-link to="/doc/switch">Switch 组件</router-link></li>
-                    <li><router-link to="/doc/button">Button 组件</router-link></li>
-                    <li><router-link to="/doc/dialog">Dialog 组件</router-link></li>
-                    <li><router-link to="/doc/tabs">Tabs 组件</router-link></li>
-                </ol>
-            </aside>
-            <main>
-                <router-view/>
-            </main>
+        <div class="layout">
+            <Topnav class="nav" />
+            <div class="content">
+                <aside v-if="asideVisible">
+                    <h2>组件列表</h2>
+                    <ol>
+                        <li><router-link to="/doc/switch">Switch 组件</router-link></li>
+                        <li><router-link to="/doc/button">Button 组件</router-link></li>
+                        <li><router-link to="/doc/dialog">Dialog 组件</router-link></li>
+                        <li><router-link to="/doc/tabs">Tabs 组件</router-link></li>
+                    </ol>
+                </aside>
+                <main>
+                    <router-view />
+                </main>
+            </div>
         </div>
     </div>
 </template>
@@ -30,14 +32,49 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+
+    >.nav {
+        flex-shrink: 0; //高度变化时不收缩
+    }
+
+    >.content {
+        flex-grow: 1; //高度变化时占据剩余空间
+        padding-top: 55px;
+        padding-left: 156px;
+
+        @media (max-width: 500px) {
+            padding-left: 0;
+        }
+    }
+}
+
 .content {
-    position: relative;
+    display: flex;
+
+    >aside {
+        flex-shrink: 0; //宽度变化时不收缩
+    }
+
+    >main {
+        flex-grow: 1; //宽度变化时占据剩余空间
+        padding: 16px;
+        background-color: lightgreen;
+    }
 }
 
 aside {
     background: lightblue;
     width: 150px;
     padding: 16px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 75px;
+    height: 100%;
 
     >h2 {
         margin-bottom: 4px;
@@ -49,11 +86,9 @@ aside {
         }
     }
 
-    @media (max-width: 500px) {
-        position: absolute;
-        top: 0;
-        left: 0;
-        padding-top: 70px;
+
+    main {
+        overflow: auto;
     }
 }
 </style>
