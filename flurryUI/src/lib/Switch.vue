e68f236c764fe8111cfa90476a28e4c072903f5d
xue0211 <zhengtai1123.qq.com>
2025-02-03 17:21:27 +0800

    添加Switch组件样式

--- flurryUI/src/lib/Switch.vue
+++ flurryUI/src/lib/Switch.vue
@@ -1,5 +1,34 @@
 <template>
-    <div>
-        Switch 组件
-    </div>
+    <button><span></span></button>
 </template>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+
+button {
+    position: relative;
+    display: inline-block;
+    width: $h * 2;
+    height: $h;
+    line-height: $h;
+    vertical-align: middle;
+    border: none;
+    background-color: #1890ff;
+    border-radius: $h/2;
+    outline: none;
+    cursor: pointer;
+    transition: all 0.2s;
+
+    >span {
+        position: absolute;
+        top: 2px;
+        left: 2px;
+        width: $h2;
+        height: $h2;
+        border: none;
+        border-radius: $h/2;
+        background-color: #fff;
+    }
+}
+</style>
