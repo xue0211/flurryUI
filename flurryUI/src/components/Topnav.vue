5585b35dcd1b1e70264df745e08b175453378798
xue0211 <zhengtai1123.qq.com>
2025-02-03 13:33:56 +0800

    Topnav封装完成

---
+++ flurryUI/src/components/Topnav.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="topnav">
+           <div class="logo">LOGO</div>
+           <ul>
+               <li>菜单</li>
+           </ul>
+           <div class="menu"></div>
+       </div>
+</template>
+<style lang="scss" scoped>
+.topnav {
+    background-color: pink;
+    display: flex;
+    padding: 16px;
+    > .logo {
+      max-width: 6em;
+      margin-right: auto;
+    }
+    > .menu {
+      display: flex;
+      white-space: nowrap;
+      flex-wrap: nowrap;
+      > li {
+        margin: 0 1em;
+      }
+    }
+  }
+</style> 
--- flurryUI/src/views/Home.vue
+++ flurryUI/src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
     <div>
-        <div class="topnav">
-            <div class="logo">LOGO</div>
-            <ul>
-                <li>菜单</li>
-            </ul>
-            <div class="menu"></div>
-        </div>
+        <Topnav/>
         <div class="banner">
             <h1>flurryUI</h1>
             <h2>一个基于Vue3.0的UI框架</h2>
@@ -17,22 +11,11 @@
         </div>
     </div>
 </template>
+<script lang="ts">
+import Topnav from '../components/Topnav.vue';
+export default{
+    components: {Topnav}
+}
+</script>
 <style lang="scss" scoped>
-.topnav {
-    background-color: pink;
-    display: flex;
-    padding: 16px;
-    > .logo {
-      max-width: 6em;
-      margin-right: auto;
-    }
-    > .menu {
-      display: flex;
-      white-space: nowrap;
-      flex-wrap: nowrap;
-      > li {
-        margin: 0 1em;
-      }
-    }
-  }
 </style>
