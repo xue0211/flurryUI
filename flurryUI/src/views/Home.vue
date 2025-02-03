4d8453266585ffde5691642aeae50b3ed67cb1d8
xue0211 <zhengtai1123.qq.com>
2025-02-03 13:10:18 +0800

    搭建HTML结构

---
+++ flurryUI/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="topnav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+        <div class="banner">
+            <h1>flurryUI</h1>
+            <h2>一个基于Vue3.0的UI框架</h2>
+            <p class="actions">
+                <a href="">开始</a>
+                <a href="">GitHub</a>
+            </p>
+        </div>
+    </div>
+</template>
