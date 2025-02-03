bcb182d6ed193cc71e1359a1c0e3005cac7d9d16
xue0211 <zhengtai1123.qq.com>
2025-02-03 19:31:11 +0800

    完成switch的css部分

--- flurryUI/src/lib/Switch.vue
+++ flurryUI/src/lib/Switch.vue
@@ -1,5 +1,13 @@
 <template>
-    <button @click="toggle" :class="{ checked:value}"><span></span></button>
+    <button 
+    class="flurry-switch" 
+    @click="toggle" 
+    :class="{ 'flurry-checked': value }"
+    >
+        <span></span>
+        <p v-if="!value">0</p>
+        <p v-else>1</p>
+    </button>
 </template>
 
 <script lang="ts">
@@ -17,12 +25,11 @@ export default {
     }
 }
 </script>
-
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
 
-button {
+.flurry-switch {
     position: relative;
     display: inline-block;
     width: $h * 2;
@@ -30,35 +37,69 @@ button {
     line-height: $h;
     vertical-align: middle;
     border: none;
-    background: #B4BEBF;
-    ;
+    background-color: #919799; //可以修改
     border-radius: $h/2;
     outline: none;
     cursor: pointer;
-    transition: all 0.2s;
+    transition: all 0.25s ease-in-out;
+
+    &:focus {
+        box-shadow: 0 0 10px rgba(191, 191, 191, 0.7);
+    }
+
+    >span {
+        position: absolute;
+        top: 2px;
+        left: 2px;
+        width: $h2;
+        height: $h2;
+        border: none;
+        border-radius: $h/2;
+        background-color: #fff; // 可以改
+        transition: all 0.25s ease-in-out;
+    }
+
+    >p {
+        display: inline-block;
+        width: 12px;
+        height: $h;
+        font-size: 12px;
+        color: #fff;   // 可以改
+        margin: 0 7px 0 25px;
+        transition: margin 0.25s ease-in-out;
+    }
+
+    &:active {
+        box-shadow: none;
+
+        >span {
+            width: $h + 2px;
+        }
+    }
 }
 
-span {
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    width: $h2;
-    height: $h2;
-    border: none;
-    border-radius: $h/2;
-    background-color: #fff;
-    transition: left 250ms;
-}
+.flurry-switch.flurry-checked {
+    background-color: #1983B5; //可以修改
 
-button.checked {
-    background: #5F83B2
-}
+    >span {
+        left: calc(100% - #{$h2} - 2px);
+    }
 
-button.checked>span {
-    left: calc(100% - #{$h2} - 2px)
-}
+    >p {
+        margin: 0 25px 0 7px;
+    }
 
-button:focus {
-    outline: none;
+    &:focus {
+        box-shadow: 0 0 10px rgba(24, 144, 255, 0.7);
+    }
+
+    &:active {
+        box-shadow: none;
+
+        >span {
+            width: $h + 2px;
+            margin-left: -6px;
+        }
+    }
 }
 </style>
