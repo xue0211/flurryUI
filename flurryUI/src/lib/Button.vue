221eb5f58015ce7cf2aee4e632326693e51b9531
xue0211 <zhengtai1123.qq.com>
2025-02-05 11:09:07 +0800

    Button支持level属性

--- flurryUI/src/lib/Button.vue
+++ flurryUI/src/lib/Button.vue
@@ -16,13 +16,18 @@ export default {
             type: String,
             default: "normal",
         },
+        level: {
+            type: String,
+            default: "normal",
+        },
     },
     setup(props) {
-        const { theme, size } = props;
+        const { theme, size, level } = props;
         const classes = computed(() => {
             return {
                 [`flurry-theme-${theme}`]: theme,
                 [`flurry-size-${size}`]: size,
+                [`flurry-level-${level}`]: level,
             };
         });
         return { classes };
@@ -40,6 +45,7 @@ $color: #333;
 $blue: #40a9ff;
 // 角度参数
 $radius: 4px;
+$red: #f56c6c;
 
 .flurry-button {
     box-sizing: border-box;
@@ -56,6 +62,7 @@ $radius: 4px;
     border: 1px solid $border-color;
     border-radius: $radius;
     box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 0.25s;
 
     //   相邻组件间相隔8px
     &+& {
@@ -109,5 +116,52 @@ $radius: 4px;
         height: 20px;
         padding: 0 4px;
     }
+    &.flurry-theme-button {
+    &.flurry-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.flurry-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.flurry-theme-link {
+    &.flurry-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.flurry-theme-text {
+    &.flurry-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.flurry-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
 }
 </style>
