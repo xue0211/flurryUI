b2373e23f40def09cb4d5e459c8e0078c8118457
xue0211 <zhengtai1123.qq.com>
2025-02-26 14:18:55 +0800

    Dialog 支持自定义 title 和 content

--- flurryUI/src/components/DialogDemo.vue
+++ flurryUI/src/components/DialogDemo.vue
@@ -2,12 +2,15 @@
     <div>Dialog 示例</div>
     <h1>示例 1</h1>
     <Button @click="toggle">toggle</Button>
-    <Dialog 
-        v-model:visible="x" 
-        :closeOnClickOverlay="false"
-        :ok="fn1"
-        :cancel="fn2"
-    ></Dialog>
+    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="fn1" :cancel="fn2">
+        <template v-slot:content>
+            <strong>hi</strong>
+            <div>hi2</div>
+        </template>
+        <template v-slot:title>
+            <strong>加粗的标题</strong>
+        </template>
+    </Dialog>
 </template>
 
 <script lang="ts">
--- flurryUI/src/lib/Dialog.vue
+++ flurryUI/src/lib/Dialog.vue
@@ -4,11 +4,11 @@
         <div class="flurry-dialog-wrapper">
             <div class="flurry-dialog">
                 <header>
-                    标题<span class="flurry-dialog-close" @click="close"></span>
+                    <slot name="title" />
+                    <span class="flurry-dialog-close" @click="close"></span>
                 </header>
                 <main>
-                    <p>第一行字</p>
-                    <p>第二行字</p>
+                    <slot name="content" />
                 </main>
                 <footer>
                     <Button level="main" @click="ok">OK</Button>
@@ -23,6 +23,10 @@
 import Button from './Button.vue';
 export default {
     props: {
+        title: {
+            type: String,
+            default: '提示'
+        },
         visible: {
             type: Boolean,
             default: false,
