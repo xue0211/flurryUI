33f647cbf0adfe92fb43d212a503074c4679238f
xue0211 <zhengtai1123.qq.com>
2025-02-26 15:27:57 +0800

    创建 Tabs 和 Tab 组件

--- flurryUI/src/components/TabsDemo.vue
+++ flurryUI/src/components/TabsDemo.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
-        Tabs 文档
+        Tabs 示例
     </div>
+    <h1>示例1</h1>
+    <Tabs>
+        <Tab title="导航1">内容1</Tab>
+        <Tab title="导航2">内容2</Tab>
+    </Tabs>
 </template>
+
+<script lang="ts">
+import Tab from '../lib/Tab.vue';
+import Tabs from '../lib/Tabs.vue';
+export default{
+    components: {
+            Tab,
+            Tabs
+    }
+}
+</script>
