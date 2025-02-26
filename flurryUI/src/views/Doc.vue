7cb78e50695e4559d3738cf9283af7628f69742f
xue0211 <zhengtai1123.qq.com>
2025-02-26 23:08:19 +0800

    路由高亮

--- flurryUI/src/views/Doc.vue
+++ flurryUI/src/views/Doc.vue
@@ -83,8 +83,8 @@ export default {
 
 aside {
     background: lightblue;
-    width: 150px;  //285px
-    padding: 16px;
+    width: 150px; //285px
+    padding: 16px 0;
     position: fixed;
     top: 0;
     left: 0;
@@ -95,11 +95,20 @@ aside {
 
     >h2 {
         margin-bottom: 4px;
+        padding: 0 16px;
     }
 
     >ol {
         >li {
-            padding: 4px 0;
+            >a{
+                display: block;
+                padding: 4px 16px;
+                text-decoration: none;
+            }
+
+            .router-link-active {
+                background: white;
+            }
         }
     }
 
