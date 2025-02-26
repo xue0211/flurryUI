dbe23956a9a9dc20d8ac538130251f1837d76ee0
xue0211 <zhengtai1123.qq.com>
2025-02-26 17:01:57 +0800

    完成了 Tab 组件，但存在异步 BUG

--- flurryUI/src/lib/Tabs.vue
+++ flurryUI/src/lib/Tabs.vue
@@ -1,25 +1,135 @@
 <template>
-    <div>
-        Tabs 组件
-        <component :is="defaults[0]" />
-        <component :is="defaults[1]" />
+    <div class="flurry-tabs">
+        <div class="flurry-tabs-nav" ref="container">
+            <div class="flurry-tabs-nav-item" v-for="(itemTitle, index) in titles" 
+            :ref="(el) => {
+                    if (itemTitle === selected) selectedItem = el;
+                }
+                " 
+                @click="select(itemTitle)" 
+                :class="{ selected: itemTitle === selected }" 
+                :key="index"
+                >
+                {{ itemTitle }}
+            </div>
+            <div class="flurry-tabs-nav-indicator" ref="indicator"></div>
+        </div>
+        <div class="flurry-tabs-content">
+            <component 
+            class="flurry-tabs-content-item"
+            :key="current.props.title" 
+            :is="current" />
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
+import { computed, ref, onMounted, watchEffect } from "vue";
 export default {
+    props: {
+        selected: {
+            type: String
+        }
+    },
     setup(props, context) {
-        const defaults = context.slots.default()
+        const selectedItem = ref<HTMLDivElement>(null);
+        const indicator = ref<HTMLDivElement>(null);
+        const container = ref<HTMLDivElement>(null);
+
+        // const x = () => {
+        //   const { width } = selectedItem.value.getBoundingClientRect();
+        //   indicator.value.style.width = width + "px";
+        //   const { left: left1 } = container.value.getBoundingClientRect();
+        //   const { left: left2 } = selectedItem.value.getBoundingClientRect();
+        //   const left = left2 - left1;
+        //   indicator.value.style.left = left + "px";
+        // };
+        onMounted(() => {
+            watchEffect(() => {
+                const { width } = selectedItem.value.getBoundingClientRect();
+                console.log(selectedItem.value);
+
+                indicator.value.style.width = width + "px";
+                const { left: left1 } = container.value.getBoundingClientRect();
+                const { left: left2 } = selectedItem.value.getBoundingClientRect();
+                const left = left2 - left1;
+                console.log(left);
+                indicator.value.style.left = left + "px";
+            });
+        });
+        // onMounted(x);
+        // onUpdated(x);
+        const defaults = context.slots.default();
         defaults.forEach((tag) => {
             if (tag.type !== Tab) {
                 throw new Error('Tabs 子标签必须是 Tab')
             }
-        })
+        });
+
+        const current = computed(() => {
+            return defaults.find((tag) => tag.props.title === props.selected);
+        });
+
+        const titles = defaults.map((tag) => {
+            return tag.props.title
+        });
+
+        const select = (title: String) => {
+            context.emit("update:selected", title);
+        };
         return {
-            defaults
+            current,
+            defaults,
+            titles,
+            select,
+            selectedItem,
+            indicator,
+            container,
         }
     }
 
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.flurry-tabs {
+    &-nav {
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+        position: relative;
+
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child {
+                margin-left: 0;
+            }
+
+            &.selected {
+                color: $blue;
+            }
+        }
+
+        &-indicator {
+            position: absolute;
+            height: 3px;
+            background: $blue;
+            left: 0;
+            bottom: -1px;
+            transition: all 0.25s;
+        }
+    }
+
+    &-content {
+        padding: 8px 0;
+    }
+}
+</style>
