a0ed86aa57e8c6acc9bf1b84817b07780e949a1e
xue0211 <zhengtai1123.qq.com>
2025-02-26 15:18:56 +0800

    实现单行代码打开Dialog

--- flurryUI/src/components/DialogDemo.vue
+++ flurryUI/src/components/DialogDemo.vue
@@ -1,32 +1,25 @@
 <template>
     <div>Dialog 示例</div>
     <h1>示例 1</h1>
-    <div style="position: relative; z-index: 1">
-        <Button @click="toggle">toggle</Button>
-        <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="fn1" :cancel="fn2">
-            <template v-slot:content>
-                <strong>hi</strong>
-                <div>hi2</div>
-            </template>
-            <template v-slot:title>
-                <strong>加粗的标题</strong>
-            </template>
-        </Dialog>
-    </div>
-    <div style="
-      position: relative;
-      z-index: 2;
-      width: 300px;
-      height: 300px;
-      background: red;
-    "
-    ></div>
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="fn1" :cancel="fn2">
+        <template v-slot:content>
+            <strong>hi</strong>
+            <div>hi2</div>
+        </template>
+        <template v-slot:title>
+            <strong>加粗的标题</strong>
+        </template>
+    </Dialog>
+    <h1>示例 2</h1>
+    <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
 import Button from '../lib/Button.vue';
-import { ref } from 'vue';
+import { ref, h} from 'vue';
+import { openDialog } from '../lib/openDialog';
 export default {
     components: {
         Dialog,
@@ -42,11 +35,24 @@ export default {
         };
         const fn2 = () => {
         };
+        const showDialog = () => {
+            openDialog({
+                title: h("strong", {}, "标题"),
+                content: '你好',
+                ok() {
+                    console.log('ok')
+                },
+                cancel(){
+                    console.log('cancel')
+                }
+            })
+        };
         return {
             x,
             toggle,
             fn1,
-            fn2
+            fn2,
+            showDialog
         };
     },
 }
--- flurryUI/src/lib/Dialog.vue
+++ flurryUI/src/lib/Dialog.vue
@@ -54,6 +54,7 @@ export default {
             }
         };
         const cancel = () => {
+            props.cancel?.();
             close();
         };
         return {
