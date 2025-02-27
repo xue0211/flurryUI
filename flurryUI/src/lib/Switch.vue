2522a300e0b069a080767f7c3b92d7d044e17412
xue0211 <zhengtai1123.qq.com>
2025-02-27 22:45:04 +0800

    抽离用户需要拷贝的代码

--- flurryUI/src/lib/Switch.vue
+++ flurryUI/src/lib/Switch.vue
@@ -1,31 +1,45 @@
 <template>
-    <button class="flurry-switch" @click="toggle" :class="{ 'flurry-checked': value }">
-        <span></span>
-        <p v-if="!value">0</p>
-        <p v-else>1</p>
-    </button>
-</template>
-
-<script lang="ts">
-import { ref } from 'vue';
-
-export default {
+    <div>
+      <button
+        class="flurry-switch"
+        @click="toggle"
+        :class="{ 'flurry-checked': value }"
+        :disabled="loading ? true : disabled"
+      >
+        <span><span class="flurry-switch-loading" v-if="loading"></span></span>
+        <p v-if="value" class="flurry-switch-on">on</p>
+        <p v-else class="flurry-switch-off">off</p>
+      </button>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { ref } from "vue";
+  export default {
     props: {
-        value: Boolean
+      value: Boolean,
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+      loading: {
+        type: Boolean,
+        default: false,
+      },
     },
     setup(props, context) {
-        const toggle = () => {
-            context.emit('update:value', !props.value)
-        }
-        return { toggle }
-    }
-}
-</script>
-<style lang="scss">
-$h: 22px;
-$h2: $h - 4px;
-
-.flurry-switch {
+      const toggle = () => {
+        context.emit("update:value", !props.value);
+      };
+      return { toggle };
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  $h: 22px;
+  $h2: $h - 4px;
+  .flurry-switch {
     position: relative;
     display: inline-block;
     width: $h * 2;
@@ -33,77 +47,99 @@ $h2: $h - 4px;
     line-height: $h;
     vertical-align: middle;
     border: none;
+    padding: 0;
     margin-bottom: 8px;
-    background-color: #919799; //可以修改
+    background-color: #bfbfbf; //#1890ff
     border-radius: $h/2;
     outline: none;
     cursor: pointer;
     transition: all 0.25s ease-in-out;
-
+    &[disabled] {
+      pointer-events: none;
+    }
+  
     &:focus {
-        box-shadow: 0 0 5px rgba(191, 191, 191, 0.5);
-
-        &:hover {
-            box-shadow: none;
-        }
+      box-shadow: 0 0 5px rgba(191, 191, 191, 0.5);
+      &:hover {
+        box-shadow: none;
+      }
     }
-
-    >span {
-        position: absolute;
-        top: 2px;
-        left: 2px;
-        width: $h2;
-        height: $h2;
-        border: none;
-        border-radius: $h/2;
-        background-color: #fff; // 可以改
-        transition: all 0.25s ease-in-out;
-    }
-
-    >p {
+    > span {
+      position: absolute;
+      top: 2px;
+      left: 2px;
+      width: $h2;
+      height: $h2;
+      border: none;
+      border-radius: $h/2;
+      background-color: #fff;
+      transition: all 0.25s ease-in-out;
+      > .flurry-switch-loading {
+        width: 14px;
+        height: 14px;
         display: inline-block;
-        width: 12px;
-        height: $h;
-        font-size: 12px;
-        color: #fff; // 可以改
-        margin: 0 7px 0 25px;
-        transition: margin 0.25s ease-in-out;
+        // margin-right: 4px;
+        border-radius: 8px;
+        border-color: lighten(#8486ab, 20%) lighten(#8486ab, 10%) #8486ab
+          transparent;
+        border-style: solid;
+        border-width: 2px;
+        animation: flurry-spin 1s infinite linear;
+      }
     }
-
+    > p {
+      display: inline-block;
+      width: 14px;
+      height: $h;
+      font-size: 14px;
+      color: #fff;
+      margin: 0 7px 0 22px;
+      transition: margin 0.25s ease-in-out;
+    }
     &:active {
-
-        >span {
-            width: $h + 2px;
-        }
+      > span {
+        width: $h + 2px;
+      }
     }
-
-
     &.flurry-checked {
-        background-color: #1983B5; //可以修改
-
-        >span {
-            left: calc(100% - #{$h2} - 2px);
+      background-color: #1890ff;
+      > span {
+        left: calc(100% - #{$h2} - 2px);
+      }
+      > p {
+        margin: 0 25px 0 7px;
+      }
+      &:focus {
+        box-shadow: 0 0 5px rgba(24, 144, 255, 0.5);
+        &:hover {
+          box-shadow: none;
         }
-
-        >p {
-            margin: 0 25px 0 7px;
+      }
+  
+      &:active {
+        > span {
+          width: $h + 2px;
+          margin-left: -6px;
         }
-
-        &:focus {
-            box-shadow: 0 0 5px rgba(24, 144, 255, 0.5);
-
-            &:hover {
-                box-shadow: none;
-            }
-        }
-
-        &:active {
-
-            >span {
-                width: $h + 2px;
-                margin-left: -6px;
-            }
-        }
+      }
     }
-}
-</style>
+    @keyframes flurry-spin {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  
+    @keyframes button-hover {
+      from {
+        transform: translateY(0);
+      }
+      to {
+        transform: translateY(-3px);
+      }
+    }
+  }
+  </style>
+  
