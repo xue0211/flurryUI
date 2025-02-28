be94836a0073afa98553be89561befdc47ce214d
xue0211 <zhengtai1123.qq.com>
2025-02-28 09:37:56 +0800

    展示源代码

--- flurryUI/src/components/Switch2Demo.vue
+++ flurryUI/src/components/Switch2Demo.vue
@@ -1,19 +1,23 @@
-<template>
-    <Switch v-model:value="bool" disabled />
-</template>
-
-<script lang="ts">
-import Switch from "../lib/Switch.vue";
-import { ref } from "vue";
-export default {
-    components: {
+<demo>
+    支持 disabled
+</demo>
+    <template>
+      <Switch v-model:value="bool" disabled />
+    </template>
+    
+    <script lang="ts">
+    import Switch from "../lib/Switch.vue";
+    import { ref } from "vue";
+    export default {
+      components: {
         Switch,
-    },
-    setup() {
+      },
+      setup() {
         const bool = ref(false);
         return {
-            bool,
+          bool,
         };
-    },
-};
-</script>
+      },
+    };
+    </script>
+    
