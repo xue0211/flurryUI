f1bcde1a83d83ba920fbe518e5848e357ef710b1
xue0211 <zhengtai1123.qq.com>
2025-02-28 10:13:04 +0800

    支持代码高亮

--- flurryUI/src/components/SwitchDemo.vue
+++ flurryUI/src/components/SwitchDemo.vue
@@ -10,7 +10,8 @@
                 <Button>查看代码</Button>
             </div>
             <div class="demo-code">
-                <pre>{{ Switch1Demo.__sourceCode }}</pre>
+                <pre class="language-html" v-html=" Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html,
+                'html')" />
             </div>
         </div>
         <div class="demo">
@@ -22,7 +23,8 @@
                 <Button>查看代码</Button>
             </div>
             <div class="demo-code">
-                <pre>{{ Switch2Demo.__sourceCode }}</pre>
+                <pre class="language-html" v-html=" Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html,
+                'html')" />
             </div>
         </div>
     </div>
@@ -32,13 +34,22 @@ import Switch from "../lib/Switch.vue";
 import Button from "../lib/Button.vue";
 import Switch1Demo from "./Switch1Demo.vue";
 import Switch2Demo from "./Switch2Demo.vue";
+import 'prismjs';
+import 'prismjs/themes/prism.css'
+const Prism = (window as any).Prism
+
 
 import { ref } from "vue";
 export default {
     components: { Button },
     setup() {
         const bool = ref(false)
-        return { bool, Switch1Demo, Switch2Demo };
+        return { 
+            bool, 
+            Switch1Demo, 
+            Switch2Demo,
+            Prism
+         };
     },
 };
 </script>
