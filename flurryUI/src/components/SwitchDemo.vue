4eb244e5088a63f9b4cffb06690661de3292cea2
xue0211 <zhengtai1123.qq.com>
2025-02-28 10:45:00 +0800

    删掉无用代码

--- flurryUI/src/components/SwitchDemo.vue
+++ flurryUI/src/components/SwitchDemo.vue
@@ -6,26 +6,18 @@
     </div>
 </template>
 <script lang="ts">
-import Switch from "../lib/Switch.vue";
-import Button from "../lib/Button.vue";
 import Switch1Demo from "./Switch1Demo.vue";
 import Switch2Demo from "./Switch2Demo.vue";
-import 'prismjs';
-import 'prismjs/themes/prism.css'
-const Prism = (window as any).Prism
 import Demo from "./Demo.vue";
 
 
 import { ref } from "vue";
 export default {
-    components: { Button, Demo },
+    components: { Demo },
     setup() {
-        const bool = ref(false)
         return { 
-            bool, 
             Switch1Demo, 
             Switch2Demo,
-            Prism
          };
     },
 };
